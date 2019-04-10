1fbb0fa7b867bb76aac96143826ff251c23082d5
Sunny Singh <ishy.singh@gmail.com>
2019-04-10 04:31:44 -0500

    Updated logo

--- resources/views/pages/how-it-works.blade.php
+++ resources/views/pages/how-it-works.blade.php
@@ -8,5 +8,11 @@
 				<hr />
 			</div>
 		</div>
+
+		<div class="row">
+			<div class="col-lg-3 col-md-3 col-sm-12 col-12">
+				<h3 class="text-center">1</h3>
+			</div>
+		</div>
 	</div>
 @endsection
