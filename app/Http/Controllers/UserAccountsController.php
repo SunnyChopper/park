cba24ab902a0afe5a5a2aa47f08c59db4544d335
Sunny Singh <ishy.singh@gmail.com>
2019-04-11 15:53:38 -0500

    ...

--- app/Http/Controllers/UserAccountsController.php
+++ app/Http/Controllers/UserAccountsController.php
@@ -122,17 +122,16 @@ class UserAccountsController extends Controller
     public function api_pay(Request $data) {
         $stripe_helper = new StripeHelper();
         return 0;
-        if ($stripe_helper->checkout($data) == 'success') {
-            $user_id = $data->user_id;
-            $parking_sessions = ParkingSession::where('customer_id', $user_id)->where('paid', 0)->get();
-            foreach ($parking_sessions as $unpaid) {
-                $unpaid->paid = 1;
-                $unpaid->save();
-            }
-            return response()->json([
-                'error' => false
-            ]);
+        $stripe_helper->checkout($data);
+        $user_id = $data->user_id;
+        $parking_sessions = ParkingSession::where('customer_id', $user_id)->where('paid', 0)->get();
+        foreach ($parking_sessions as $unpaid) {
+            $unpaid->paid = 1;
+            $unpaid->save();
         }
+        return response()->json([
+            'error' => false
+        ]);
     }
 
     public function login(Request $data) {
